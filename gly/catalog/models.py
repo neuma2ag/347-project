0c3a31a8bfc9d6a2901590787fe0248a53d0b649
Andrew G. Neumann <neuma2ag@dukes.jmu.edu>
2025-05-07 00:02:30 -0400

    Add tags to recipe blocks in index.html

--- gly/catalog/models.py
+++ gly/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 from django.urls import reverse
+import json
 
 
 class Tag(models.Model):
--- gly/catalog/views.py
+++ gly/catalog/views.py
@@ -8,10 +8,12 @@ from .models import Tag, Recipe
 def index(request):
     num_tags = Tag.objects.all().count()
     num_recipes = Recipe.objects.all().count()
+    recipes = Recipe.objects.all()
 
     context = {
         'num_tags': num_tags,
         'num_recipes': num_recipes,
+        'recipes': recipes,
     }
 
     return render(request, 'index.html', context=context)
