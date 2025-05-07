67b800e7b6e362d5da0d083b6bcd1114db58ee9c
Andrew G. Neumann <neuma2ag@dukes.jmu.edu>
2025-05-06 22:11:40 -0400

    Implement basic models for tags and recipes

--- gly/catalog/models.py
+++ gly/catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+from django.urls import reverse
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32, unique=True,
+                            help_text="Enter a tag for recipes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular tag instance."""
+        return reverse('tag-detail', args=[str(self.id)])
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='tag_name_case_insensitive_unique',
+                violation_error_message="Tag already exists (case insensitive match)"
+            ),
+        ]
+
+
+class Recipe(models.Model):
+    title = models.CharField(
+        max_length=128, help_text="Enter the title of the recipe")
+
+    prep_time = models.IntegerField(help_text="Enter the minutes of prep time")
+
+    cook_time = models.IntegerField(help_text="Enter the minutes of cook time")
+
+    servings = models.IntegerField(help_text="Enter the number of servings")
+
+    tag = models.ManyToManyField(Tag, help_text="Select a tag for this recipe")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('recipe-detail', args=[str(self.id)])
