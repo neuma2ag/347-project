f98ac3ebc305a07c300952fd8818147ab8a83839
Andrew G. Neumann <neuma2ag@dukes.jmu.edu>
2025-05-06 23:30:13 -0400

    Add a basic home page

--- gly/catalog/views.py
+++ gly/catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Tag, Recipe
+
 # Create your views here.
+
+
+def index(request):
+    num_tags = Tag.objects.all().count()
+    num_recipes = Recipe.objects.all().count()
+
+    context = {
+        'num_tags': num_tags,
+        'num_recipes': num_recipes,
+    }
+
+    return render(request, 'index.html', context=context)
